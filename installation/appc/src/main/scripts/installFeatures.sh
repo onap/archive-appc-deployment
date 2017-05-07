45868047871fb6234b9c79568fe65d506128458e
Guo Ruijing <ruijing.guo@intel.com>
2017-05-07 21:06:18 +0000

    update copyright to avoid noisy diff after build
    Change-Id: I50d213f4d22dff2e211258a3e536dc1a50618b78
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- installation/appc/src/main/scripts/installFeatures.sh
+++ installation/appc/src/main/scripts/installFeatures.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+###
+# ============LICENSE_START=======================================================
+# openECOMP : APP-C
+# ================================================================================
+# Copyright (C) 2017 OpenECOMP
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+###
+
 ODL_HOME=${ODL_HOME:-/opt/opendaylight/current}
 APPC_HOME=${APPC_HOME:-/opt/openecomp/appc}
 APPC_FEATURE_DIR=${APPC_FEATURE_DIR:-${APPC_HOME}/features}
