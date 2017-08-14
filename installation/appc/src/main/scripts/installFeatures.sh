056b839d2c77124137e7b65c4c3af1b6fa6049e5
Hao Kuang <Hao.Kuang@amdocs.com>
2017-08-14 18:05:52 +0000

    OAM operations - deployment
    Add new APPC features for oam, command-executor and lifecycle-management.
    Includes these user stories: APPC-39/41/43/44/45/46/52/77.
    
    Issue Id: APPC-38
    Change-Id: I374fb363a46158fbf867228857c932b4da553fac
    Signed-off-by: Hao Kuang <Hao.Kuang@amdocs.com>

--- installation/appc/src/main/scripts/installFeatures.sh
+++ installation/appc/src/main/scripts/installFeatures.sh
@@ -42,17 +42,21 @@ done
 APPC_FEATURES=" \
  appc-metric \
  appc-provider \
+ appc-command-executor \
  appc-dmaap-adapter \
  appc-event-listener \
  appc-chef-adapter \
  appc-netconf-adapter \
  appc-rest-adapter \
+ appc-lifecycle-management \
  appc-dispatcher \
  appc-dg-util \
  appc-dg-shared \
  appc-asdc-listener \
+ appc-oam \
  appc-iaas-adapter \
  appc-ansible-adapter"
+
 
 echo "Enabling core APP-C features"
 featureInstall odl-netconf-connector-all
